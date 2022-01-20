3306702ef87ee20c2bc69ec4eeb4ca92e0c985af
ofirsalem1 <89575154+ofirsalem1@users.noreply.github.com>
2022-01-20 23:56:58 +0200

    adding side nav

--- front/src/components/NavBar.tsx
+++ front/src/components/NavBar.tsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 const NavBar = () => {
+  const mySidenav: any = useRef();
+  console.log(mySidenav.current);
+
+  /* Set the width of the side navigation to 250px */
+  const openNav = () => {
+    mySidenav.current.style.width = '250px';
+  };
+  /* Set the width of the side navigation to 0 */
+  const closeNav = () => {
+    mySidenav.current.style.width = '0';
+  };
   return (
-    <div className="nav-bar-div">
-      <ul className="nav-bar-ul">
-        <li>Home</li>
-        <li>Word</li>
-        <li>Word With Pos</li>
-        <li>Random From Pos</li>
-        <li>Start With From Pos</li>
-      </ul>
+    <div>
+      <div ref={mySidenav} id="mySidenav" className="sidenav">
+        <a href="javascript:void(0)" className="closebtn" onClick={closeNav}>
+          &times;
+        </a>
+        <a href="#">About</a>
+        <a href="#">Services</a>
+        <a href="#">Clients</a>
+        <a href="#">Contact</a>
+      </div>
+      {/* // Use any element to open the sidenav */}
+      <span onClick={openNav}>open</span>
+      {/* // Add all page content inside this div if you want the side nav to push page content to the
+      right (not used if you only want the sidenav to sit on top of the page */}
     </div>
   );
 };
