af102f533d3e8b3490a6f05aead2fac38a25c4d8
ofirsalem1 <89575154+ofirsalem1@users.noreply.github.com>
2022-01-20 15:35:44 +0200

    find Word From Part OF_SPEECH_DICT

--- backend/routes/wordsRouter.js
+++ backend/routes/wordsRouter.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 //FUNCTIONS
-const { findWord } = require('../controllers/wordsController');
+const { findWord, findWordFromPart } = require('../controllers/wordsController');
 
 // If word has more than one parts of speech
 // will return all words part of speech, else,
 // will return a word + definition + part of speech.
 router.get('/:word', findWord);
+router.get('/:word/:partOfSpeech', findWordFromPart);
 
 module.exports.wordsRouter = router;
