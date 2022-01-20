15ac400f1e38b712f52f3ea18b4ce782b04804b2
ofirsalem1 <89575154+ofirsalem1@users.noreply.github.com>
2022-01-20 18:20:11 +0200

    search by pos and letter

--- backend/controllers/partOfSpeechController.js
+++ backend/controllers/partOfSpeechController.js
@@ -4,24 +4,24 @@ const { TABLE_NAME, PART_OF_SPEECH_DICT } = require('../utils/constants');
 
 exports.randomByPos = async (req, res, next) => {
   const { part } = req.params;
-  //   const params = {
-  //     TableName: TABLE_NAME,
-  //     KeyConditionExpression: `pos = :pos`,
-  //     ExpressionAttributeValues: {
-  //       ':pos': PART_OF_SPEECH_DICT[part],
-  //     },
-  //   };
-
-  var params = {
+  const { letter } = req.query;
+  let params;
+  console.log('not letter');
+  params = {
     TableName: TABLE_NAME,
-    FilterExpression: '#pos = :pos', // optional
-    ExpressionAttributeValues: { ':pos': PART_OF_SPEECH_DICT[part] },
-    ExpressionAttributeNames: { '#pos': 'pos' }, // optional
+    FilterExpression: '#pos = :pos and begins_with(#word, :word)',
+    ExpressionAttributeNames: {
+      '#pos': 'pos',
+      '#word': 'word',
+    },
+    ExpressionAttributeValues: {
+      ':pos': PART_OF_SPEECH_DICT[part],
+      ':word': letter ? letter.toUpperCase() : '',
+    },
   };
-
   try {
+    console.log(params);
     const result = await dynamodb.scan(params).promise();
-    console.log(result.Items.length);
     const randomNum = Math.floor(Math.random() * result.Items.length);
     return res.status(200).json(result.Items[randomNum]);
   } catch (error) {
