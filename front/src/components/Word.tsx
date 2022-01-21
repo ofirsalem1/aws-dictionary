8883402f5963dae002917cc002506ea1a73d1bdf
ofirsalem1 <89575154+ofirsalem1@users.noreply.github.com>
2022-01-21 13:50:14 +0200

    search word by search input

--- front/src/components/Word.tsx
+++ front/src/components/Word.tsx
@@ -1,37 +1,46 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const Word = () => {
+  const navigate = useNavigate();
   const [wordDefinition, setWordDefinition] = useState<[Object] | null>(null);
   const { word } = useParams();
+  const [searchInput, setSearchInput] = useState<string>('');
   useEffect(() => {
-    getWord();
+    if (word) {
+      getWord(word);
+    }
   }, []);
 
-  const getWord = async () => {
-    const response = await axios.get(`http://localhost:3000/${word}`);
+  const getWord = async (searchWord: string) => {
+    const response = await axios.get(`http://localhost:3000/${searchWord}`);
+    navigate(`/word/${searchWord}`);
     setWordDefinition(response.data);
   };
-  console.log(wordDefinition);
   return (
     <div>
       <h2>Search By Word</h2>
-      <input type="text" />
+      <input
+        type="text"
+        placeholder="Search Word..."
+        onChange={e => setSearchInput(e.target.value)}
+      />
+      <button onClick={() => getWord(searchInput)}>Search</button>
       {wordDefinition &&
         wordDefinition.map((word: any, i: number) => {
           return (
             <div className="definition-div" key={i}>
               <h3>
-                {word.word}, {word.pos}{' '}
+                {word.word}, {word.pos}
               </h3>
               {word.definitions.map((definition: string, i: number) => {
                 const definitionArr = definition.split(' ');
                 return (
-                  <p>
+                  <p key={i}>
                     {i + 1}:
-                    {definitionArr.map((definitionWord: string) => {
-                      return <span>{definitionWord} </span>;
+                    {definitionArr.map((definitionWord: string, i: number) => {
+                      return <span key={i}>{definitionWord} </span>;
                     })}
                   </p>
                 );
