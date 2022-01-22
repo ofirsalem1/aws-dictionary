bc0b8b1497cb2140130bf5265d6afefc4e6d97c8
ofirsalem1 <89575154+ofirsalem1@users.noreply.github.com>
2022-01-22 20:16:12 +0200

    add some style

--- front/src/components/HomePage.tsx
+++ front/src/components/HomePage.tsx
@@ -2,17 +2,15 @@ import React from 'react';
 
 const HomePage = () => {
   return (
-    <div>
+    <div className="homepage-div">
       <h3>Home Page</h3>
-      <p>
-        In this dictionary you can search by:
-        <ul>
-          <li>Word</li>
-          <li>Word with a specific part of speech</li>
-          <li>A random word from a specific part of speech</li>
-          <li>A random word from a specific part of speech and letter begin with</li>
-        </ul>
-      </p>
+      In this dictionary you can search by:
+      <ul>
+        <li>Word</li>
+        <li>Word with a specific part of speech</li>
+        <li>A random word from a specific part of speech</li>
+        <li>A random word from a specific part of speech and letter begin with</li>
+      </ul>
     </div>
   );
 };
